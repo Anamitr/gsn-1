fbef7d3a9bf02c2ef70e8017cf27e52d3ad76999
Konrad Sochacki <sochacki.konrad@gmail.com>
2019-12-11 11:34:36 +0100

    Add script for building model recognizing cats and dogs based on dataset from net

---
+++ cats_vs_dogs/cats_vs_dogs.py
@@ -0,0 +1,121 @@
+from random import seed, random
+from shutil import copyfile
+import os
+import sys
+from matplotlib import pyplot
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import tfinit
+from datetime import datetime
+
+folder = 'dogs-vs-cats/train/'
+
+tfinit.tfinit()
+
+
+def prepere_dataset():
+    dataset_home = 'dataset_dogs_vs_cats/'
+    seed(1)
+    # define ratio of pictures to use for validation
+    val_ratio = 0.25
+    # copy training dataset images into subdirectories
+    src_directory = folder
+    for file in os.listdir(src_directory):
+        src = src_directory + '/' + file
+        dst_dir = 'train/'
+        if random() < val_ratio:
+            dst_dir = 'test/'
+        if file.startswith('cat'):
+            dst = dataset_home + dst_dir + 'cats/' + file
+            copyfile(src, dst)
+        elif file.startswith('dog'):
+            dst = dataset_home + dst_dir + 'dogs/' + file
+            copyfile(src, dst)
+
+
+def define_one_block_model():
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same',
+                     input_shape=(200, 200, 3)))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
+    model.add(Dense(1, activation='sigmoid'))
+    # compile model
+    opt = SGD(lr=0.001, momentum=0.9)
+    model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
+    return model
+
+
+def define_three_block_model():
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same',
+                     input_shape=(200, 200, 3)))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
+    model.add(Dense(1, activation='sigmoid'))
+    # compile model
+    opt = SGD(lr=0.001, momentum=0.9)
+    model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
+    return model
+
+
+# plot diagnostic learning curves
+def summarize_diagnostics(history):
+    # plot loss
+    pyplot.subplot(211)
+    pyplot.title('Cross Entropy Loss')
+    pyplot.plot(history.history['loss'], color='blue', label='train')
+    pyplot.plot(history.history['val_loss'], color='orange', label='test')
+    # plot accuracy
+    pyplot.subplot(212)
+    pyplot.title('Classification Accuracy')
+    pyplot.plot(history.history['accuracy'], color='blue', label='train')
+    pyplot.plot(history.history['val_accuracy'], color='orange', label='test')
+    # save plot to file
+    filename = sys.argv[0].split('/')[-1]
+    pyplot.savefig(filename + '_plot.png')
+    pyplot.close()
+
+
+# run the test harness for evaluating a model
+def run_test_harness():
+    # define model
+    model = define_three_block_model()
+    # create data generator
+    datagen = ImageDataGenerator(rescale=1.0 / 255.0)
+    # prepare iterators
+    train_it = datagen.flow_from_directory('dataset_dogs_vs_cats/train/',
+                                           class_mode='binary', batch_size=16, target_size=(200, 200))
+    test_it = datagen.flow_from_directory('dataset_dogs_vs_cats/test/',
+                                          class_mode='binary', batch_size=16, target_size=(200, 200))
+    # fit model
+    history = model.fit_generator(train_it, steps_per_epoch=len(train_it),
+                                  validation_data=test_it, validation_steps=len(test_it), epochs=20,
+                                  use_multiprocessing=True)
+    model.summary()
+    model.save("latest_model.h5")
+    model.save("model " + str(datetime.now()) + ".h5")
+
+    # evaluate model
+    _, acc = model.evaluate_generator(test_it, steps=len(test_it), verbose=0)
+    print('> %.3f' % (acc * 100.0))
+    # learning curves
+    summarize_diagnostics(history)
+
+    return model
+
+
+# entry point, run the test harness
+model = run_test_harness()
